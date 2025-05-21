f16e2592b2d5f61402b7165d4f11b453bbe24daa
root <root@cloud20250318618.lanit.com.vn>
2025-05-21 02:30:15 +0200

    Thêm Edge Function

---
+++ api/edge.js
@@ -0,0 +1,1 @@
+export const config = { runtime: "edge" }; export default async function handler(req) { return new Response(JSON.stringify({ message: "Hello from Vercel Edge Function!" }), { status: 200, headers: { "content-type": "application/json" } }); }
