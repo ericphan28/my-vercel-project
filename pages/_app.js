6d772c67f51cd39bb8e440235c1624e646f138f2
root <root@cloud20250318618.lanit.com.vn>
2025-05-21 05:46:36 +0200

    Thêm Speed Insights

--- pages/_app.js
+++ pages/_app.js
@@ -1,1 +1,1 @@
-import { Analytics } from "@vercel/analytics/react"; export default function App({ Component, pageProps }) { return ( <> <Component {...pageProps} /> <Analytics /> </> ); }
+import { Analytics } from "@vercel/analytics/react"; import { SpeedInsights } from "@vercel/speed-insights/next"; export default function App({ Component, pageProps }) { return ( <> <Component {...pageProps} /> <Analytics /> <SpeedInsights /> </> ); }
